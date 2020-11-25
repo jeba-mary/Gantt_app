981da8da521c6b4b3653509210d48b727bb7896b
Maryjeba <maryjeba11@gmail.com>
2020-11-25 21:03:37 +0530

    integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bryntum-gantt/gantt.stockholm.css';
 import './App.css';
+import BryntumGantt from './components/BryntumGantt';
+import { ProjectModel } from 'bryntum-gantt';
+import EditComponent  from './components/EditComponent';
+import DemoEditor from './components/DemoEditer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  handleEditClick = record => {
+    this.refs.gantt.ganttInstance.editTask(record);
+  };
+  
+  render() {
+    const taskRenderer = ({ taskRecord }) => {
+      if (taskRecord.isLeaf && !taskRecord.isMilestone) {
+          return taskRecord.name;
+      }
+    }
+
+    const project = new ProjectModel({
+      transport : {
+          load : {
+              url : './data/tasks.json'
+          }
+      }
+    });
+    project.load();
+    return (
+      <>
+      <BryntumGantt
+      project      = {project}
+      taskRenderer = {taskRenderer}
+      columns      = {[
+        { type : 'name', field : 'name', width : 250 },
+        { text     : 'Edit<div class="small-text">(React component)</div>',
+                    width    : 120,
+                    editor   : false,
+                    align    : 'center',
+
+                    // Using custom React component
+                    renderer : ({ record }) => record.isLeaf ?
+                        <EditComponent
+                            text={'Edit'}
+                            onClick={() => this.handleEditClick(record)
+                        }/>
+                        : null},
+        {
+                    field    : 'draggable',
+                    text     : 'Draggable<div class="small-text">(React editor)</div>',
+                    align    : 'center',
+                    width    : 120,
+                    renderer : ({ value }) => value ? 'Yes' : 'No',
+                    editor   : ref => <DemoEditor ref={ref}/>
+                },
+      ]}
+    />
+    </>
+    )
+  }
 }
 
 export default App;
